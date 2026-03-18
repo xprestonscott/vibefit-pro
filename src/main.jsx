b9396b0698ef5726eeb513e22d26eb9e8de8334d
Preston Darnell <prestondarnell46@gmail.com>
2026-03-18 18:56:14 -0500

    Fix ErrorBoundary import

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-createRoot(document.getElementById('root')).render(<StrictMode><ErrorBoundary><App /></ErrorBoundary></StrictMode>)
+import ErrorBoundary from './components/ErrorBoundary.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
+  </StrictMode>
+)
 
 // Register service worker for PWA
 if ('serviceWorker' in navigator) {
